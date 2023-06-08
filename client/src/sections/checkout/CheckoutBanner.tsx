176c341d1e625e761674b55228f71842c3a125a4
huybq_107 <64139999+huybq100701@users.noreply.github.com>
2023-06-08 23:09:29 +0700

    fix

--- client/src/sections/checkout/CheckoutBanner.tsx
+++ client/src/sections/checkout/CheckoutBanner.tsx
@@ -4,11 +4,7 @@ const CheckoutBanner = () => {
   return (
     <div className="CheckoutBanner">
       <span className="logo logo-left">
-        <img
-          className="logo-image"
-          src="https://cdn.shopify.com/s/files/1/0055/9815/0690/files/002_RG_2021_FULL_LOCKUP_BLACK.png?255"
-          alt="Riot Games Merchandise"
-        />
+       
       </span>
     </div>
   );
